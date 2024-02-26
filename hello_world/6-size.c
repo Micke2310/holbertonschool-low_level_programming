e0f09e240fb71b5b998de05ed6cfcf9ed1fb03e4
Micke2310 <maikyarachan@gmail.com>
2024-02-26 04:36:16 +0000

    xx

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,13 +8,22 @@
 
 int main(void)
 {	
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("Size of a char : ");
+	printf("%zu bytes\n",sizeof(char));
+
+	puts("Size of an int: ");
+	printf("%zu bytes\n",sizeof(int));
+
+ 	puts("Size of a long int: ");
+	printf("%zu bytes\n",sizeof(long));
+
+	puts("Size of a long long int: ");
+	printf("%zu bytes\n",sizeof(long long));
+
+	puts("Size of a float: ");
+	printf("%zu bytes\n",sizeof(float));
 
 
 
-
-
-
-	
 	return (0);
 }

f013c548ed42a38586844e89143ae03bba8313a9
Micke2310 <maikyarachan@gmail.com>
2024-02-26 16:49:53 +0000

    6.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,7 +5,7 @@
  *Return: Always 0 (Succes)
  */
 
-int main(void)
+int main(void) 
 {
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
@@ -14,5 +14,5 @@ int main(void)
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 
-	return(0);
+	return (0);
 }

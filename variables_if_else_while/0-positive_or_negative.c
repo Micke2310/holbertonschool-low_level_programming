3fca3417b63a9042af6839860ca9bbc1a7d1cdc4
Micke2310 <maikyarachan@gmail.com>
2024-02-27 14:48:31 +0000

    0-positive_or_negative.c

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+# include <stdio.h>
+
+# include <time.h>
+/**
+ *main - Enter point
+ *
+ *name the file
+ *0-positive_or_negative.c
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+
+
+
+	return (0);
+
+}

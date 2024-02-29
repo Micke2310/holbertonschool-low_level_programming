82fc0e749b14d074076da13b38ecbff03868c98e
Micke2310 <maikyarachan@gmail.com>
2024-02-29 02:24:09 +0000

    2-print_alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -4,28 +4,22 @@
 
 /**
  * main - Entry point
- * more headers goes there
- * 2-print_alphabet.c
- * betty style doc for function main goes there
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-int n;
-char alphabet = "a";
+	int n;
+	char alphabet = 'a';
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-while (alphabet <= "z")
-{
-putchar(alphabet);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	while (alphabet <= 'z')
+	{
+	putchar(alphabet);
+	alphabet++;
+	}
 
-alphabet = +1;
+	putchar('\n');
 
-}
-
-putchar("\n");
-
-return (0);
+	return (0);
 }

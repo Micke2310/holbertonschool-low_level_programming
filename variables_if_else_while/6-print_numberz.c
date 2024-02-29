92b84644ef6cb3b3280a9b5bcc6241b16958a352
Micke2310 <maikyarachan@gmail.com>
2024-02-29 18:58:45 +0000

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - Enter point
+ *
+ *6-print_numberz.c
+ *
+ *Return: Always o (Success)
+ */
+
+
+int main(void)
+{
+int n = 0;
+
+while (n >= 0 && n <= 0)
+{
+putchar(n);
+n++;
+}
+putchar('\n');
+
+return (0);
+}

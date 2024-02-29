d5c8a7258c9ef8d5e7c48640797547b175643418
Micke2310 <maikyarachan@gmail.com>
2024-02-29 20:23:09 +0000

    print_alphabet

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - Enter point
+ *
+ *9-print_comb.c
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+
+
+
+
+
+return (0);
+}

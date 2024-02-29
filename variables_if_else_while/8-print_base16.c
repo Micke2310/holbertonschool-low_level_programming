653c39111a11b1383d43f2a922c706cc6acd046f
Micke2310 <maikyarachan@gmail.com>
2024-02-29 19:59:50 +0000

    8-print_base16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/*
+ *
+ *main - Enter point
+ *
+ *8-print_base16.c
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main (void)
+{
+
+	int n= 0;
+	char alphabet= 'a';
+while (n >= 0 && n <=9)
+{
+putchar(n + '0');
+n++;
+}
+
+while (alphabet >= 'a' && alphabet <= 'd')
+{
+putchar(alphabet);
+alphabet++;
+}
+putchar('\n');
+
+return (0);
+}

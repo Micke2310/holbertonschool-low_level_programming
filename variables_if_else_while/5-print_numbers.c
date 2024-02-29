30a2c8abee509bab2fe6db87e2adecaa5dc4d228
Micke2310 <maikyarachan@gmail.com>
2024-02-29 18:29:36 +0000

    5-print_numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - Enter point
+ *
+ *
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int n;
+
+while (n <= 0 && n >= 9)
+{
+printf("%d",n);
+n++;
+}
+
+
+
+
+
+
+
+return (0);
+}

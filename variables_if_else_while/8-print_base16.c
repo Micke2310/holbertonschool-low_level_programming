8eef0cf3b7fedb6bd9dc83cc8b25a9b40c357434
Micke2310 <maikyarachan@gmail.com>
2024-02-29 20:03:52 +0000

    8-print_base16

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -11,12 +11,12 @@
  *Return: Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
 
-	int n= 0;
-	char alphabet= 'a';
-while (n >= 0 && n <=9)
+	int n = 0;
+	char alphabet = 'a';
+while (n >= 0 && n <= 9)
 {
 putchar(n + '0');
 n++;

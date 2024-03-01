8ef384219add53855fb157f34d55e05dd4c25954
Micke2310 <maikyarachan@gmail.com>
2024-03-01 18:07:38 +0000

    0-putchar.c

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,10 +1,17 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef MAIN_H /** Por convencion , el nombre va en mayuscula y con guiones*/
+#define MAIN_H /** El nombre es igual al del archivo .h pero en mayusculas*/
+
+/** aqui se escriben los prototipos de funciones*/
+
+
+
+
+
 
 
 
 the entire header file file 
 
 
-
+/** debajo de esta linea no se escribe codigo*/
 #endif

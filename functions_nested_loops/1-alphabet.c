125a2e1d7070a6f6c1fb45fd42992bea32412716
Micke2310 <maikyarachan@gmail.com>
2024-03-03 16:10:46 +0000

    1-alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -5,19 +5,21 @@
 *
 *
 *
-*Return:no is necessary because is VOID the type de return, only needs in funcion main your retur is INT (number entire)
+*Return:no is necessary because is VOID the type de return,
+*only needs in funcion main your retur is INT (number entire)
 */
 
 void print_alphabet(void)
 {
 char alphabet = 'a';
 
-while ( alphabet <= 'z')
+while (alphabet <= 'z')
 {
 	_putchar(alphabet);
 	alphabet++;
 }
 _putchar('\n');
 
-/**return (0); Return:no is necessary because is VOID the type de return, only needs in funcion main your retur is INT (number entire)*/
+/**return (0); Return:no is necessary because is VOID the type de return,
+ *only needs in funcion main your retur is INT (number entire)*/
 }

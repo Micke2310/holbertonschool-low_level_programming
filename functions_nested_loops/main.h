d1c567920e0e53f1e24769dd63363799397f4949
Micke2310 <maikyarachan@gmail.com>
2024-03-03 05:29:50 +0000

    0-putchar

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,29 +1,7 @@
-#ifndef MAIN_H /** Por convencion , el nombre va en mayuscula y con guiones*/
-#define MAIN_H /** El nombre es igual al del archivo .h pero en mayusculas*/
+#ifndef MAIN_H
+#define MAIN_H
 
-/** aqui se escriben los prototipos de funciones*/
+int _putchar(char c);
 
-int putchar(char);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
+#endif /* MAIN_H */
 
-
-
-
-
-
-
-the entire header file file 
-
-
-/** debajo de esta linea no se escribe codigo*/
-#endif

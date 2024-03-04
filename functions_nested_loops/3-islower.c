a658e4b064ab3459cb1333772fa35ac77dadb654
Micke2310 <maikyarachan@gmail.com>
2024-03-04 14:15:47 +0000

    3-islower.c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-*int _islower(int c) - enter point.
-*_islower funcion is greate.
-*'c' parameter is int.
-*Description: when c is lowercase return 1, otherwise return 0
-*Return: 1 is lowercase, 0 otherwise
-*/
+ *int_islower(int c) - enter point.
+ *_islower funcion is greate, verifica un caracter en minuscula
+ *'c' parameter is int, es el caracter a verificar
+ *Description: when c is lowercase return 1, otherwise return 0
+ *Return: 1 is lowercase, 0 otherwise
+ */
 
 
 int _islower(int c)

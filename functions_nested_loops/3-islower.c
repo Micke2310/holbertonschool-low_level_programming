cde79a88ed3e19e69374641ef565b0e5a766f422
Micke2310 <maikyarachan@gmail.com>
2024-03-04 14:21:05 +0000

    3-islower.c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * _islower - Verifica si un carácter es minúscu
- * @c: El carácter a verifica
+ * _islower - Verifica si un carácter es minúsla
+ * @c: El carácter a verificr
  *
- * Return: 1 si c es una minúscula, 0 en caso contrari
+ * Return: 1 si c es una minúscula, 0 en caso contraro.
  *
- * Descripción: La función _islower verifica si un carácter dado es una letra minúsc
- *              Devuelve 1 si el carácter es una minúscula y 0 si no lo e
+ * Descripción
+ * La función _islower verifica si un carácter dado es una letra miuscula.
+ * Devuelve 1 si el carácter es una minúscula y 0 si no los.
  */
 
 int _islower(int c)

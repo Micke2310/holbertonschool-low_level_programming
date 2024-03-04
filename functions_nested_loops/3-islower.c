170977399cda94a9c56213f503135dbb56c612f4
Micke2310 <maikyarachan@gmail.com>
2024-03-04 13:38:08 +0000

    3-islower.c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,18 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "main.h"
 
 /**
-*main - Enter point
+*int _islower(int c)
 *
-*
-*
-*Retur: Always 0 (Success)
+*Return: 1 is lowercase, 0 otherwiae
 */
 
-int main(void)
+
+int _islower(int c)
 {
 
+char lowercase = 'a';
+
+if (lowercase => 'a' && lowercase <= 'z')
+{
+_putchar(lowercase + '1');
+_putchar++;
+}
+else if (lowercase < 'a' || lowercase > 'z')
+{
+_putchar(lowercase + '0');
+_putchar++;
+
+}
 
 
 return (0);

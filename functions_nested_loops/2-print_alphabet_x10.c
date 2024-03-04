72316b8d6db821547eee7126f2468e2295675146
Micke2310 <maikyarachan@gmail.com>
2024-03-04 06:50:34 +0000

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -10,19 +10,20 @@ void print_alphabet_x10(void)
 {
 char alphabet = 'a';
 int i;
-for( i = 0; i < 10; i++)
+for (i = 0; i < 10; i++)
 {
-while( alphabet <= 'z')
+while (alphabet <= 'z')
 {
 _putchar(alphabet);
 alphabet++;
 }
 _putchar('\n');
 alphabet = 'a';
-/**reiniciamos el valor de alphabet,
- * para comenzar de nuevo tras cda iteracion, por 10 veces.
+
+/**
+ *reiniciamos el valor de alphabet,
+ *para comenzar de nuevo tras cda iteracion, por 10 veces.
  *
  */
-
 }
 }

2f2ea1240639fdc55953d8a94a2e3453d678331a
Micke2310 <maikyarachan@gmail.com>
2024-03-04 06:39:39 +0000

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,27 +1,25 @@
 #include "main.h"
 
 /**
-*void print_alphabet_x10(void)
-*
-*
-*
-*Return: no tieene (Success)
-*/
+ *void print_alphabet_x10(void)
+ *
+ *Return: no tieene (Success)
+ */
 
 void print_alphabet_x10(void)
 {
-
-	char alphabet = 'a';
-int i = 0;
- 
-while( alphabet <= 'z')
+char alphabet = 'a';
+int i;
+for( i = 0; i < 10; i++)
 {
-	for(i = 0; i < 10; i++)
-	{
-	_putchar(alphabet);
+while(alphabet <= 'z')
+{
+_putchar(alphabet);
 alphabet++;
-	}
 }
 _putchar('\n');
-
+alphabet = 'a';
+/**reiniciamos el valor de alphabet,
+ * para comenzar de nuevo tras cda iteracion, por 10 veces.*/
+}
 }

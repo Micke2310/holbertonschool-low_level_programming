02bcb74ab9a893bef7dad4771387118ca8fdbda4
Micke2310 <maikyarachan@gmail.com>
2024-03-04 17:18:40 +0000

    5-sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- *print_sign - es una funcion que verifica si los caracterse son signos.
+ * print_sing - imprime el signo de un numero
+ * @n: el numero a verificar
  *
- *@'n' es un argumeto de tipo int, el cual es comparado por print_sign.
- *
- *Retur: 1 si n es mayor que cero, -1 si es menor que 0 y 0 si es igual a 0.
+ * Return: 1 si n es positivo, -1 si es negativo y 0 si es 0.
  */
+
+
 
 int print_sign(int n)
 {

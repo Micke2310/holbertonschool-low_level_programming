7a407d992aa8403d01ae77f1b0590e26a633300c
Micke2310 <maikyarachan@gmail.com>
2024-03-04 06:57:00 +0000

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -2,9 +2,14 @@
 
 /**
  *void print_alphabet_x10(void)
- *Description
+ *Description: imprime alphabeto 10 veces
  *Return: no tieene (Success)
+ *2-print_alphabet_x10.c - Programa para imprimir el alfabeto 10 veces
+ *
  */
+
+
+
 
 void print_alphabet_x10(void)
 {

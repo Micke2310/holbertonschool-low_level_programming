043af5695d637df2b247aedaf34df0534ed5c0f5
Micke2310 <maikyarachan@gmail.com>
2024-03-04 07:04:17 +0000

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - Imprime el alfabeto 10 
- * veces seguido de un salto de línea
+ * print_alphabet_x10 - Imprime el alfabeto 10
+ * veces seguido de un salto de líne
  *
- * Esta función imprime el alfabeto en minúscula
+ * Esta función imprime el alfabeto en minúscu
  * s desde 'a' hasta 'z' diez veces, con cada
- * alfabeto impreso en una línea separada.
- * 2-print_alphabet_x10.c - 
+ * alfabeto impreso en una línea separada
+ * 2-print_alphabet_x10.c -
  * Programa para imprimir el alfabeto 10 veces
  *
- * Este programa define la función print_alphabet_x10,
- * que imprime el alfabeto en minúsculas
- * diez veces, con cada alfabeto impreso en una línea separada.
+ * Este programa define la función print_alphabet_x10
+ * que imprime el alfabeto en minúscula
+ * diez veces, con cada alfabeto impreso en una línea separada
  */
 
 

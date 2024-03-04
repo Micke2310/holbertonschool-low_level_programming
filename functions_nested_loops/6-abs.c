c790555e780f42b549fbd2e5fe47c828d15a5929
Micke2310 <maikyarachan@gmail.com>
2024-03-04 18:26:44 +0000

    6-abs

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * _abs - calcula el valor absoluto de un int
- * @"number": este valor es el entero del que se calculara su valor
+ * @number: este valor es el entero del que se calculara su valor
  *
- * Return: el valor absoluto de "number".
+ * Return: el valor absoluto de 'number'.
  */
 
 int _abs(int number)

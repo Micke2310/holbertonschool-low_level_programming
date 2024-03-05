a73892e449a2cfd98b91a761c3cb00299b28bf3e
Micke2310 <maikyarachan@gmailcom>
2024-03-05 21:23:52 +0000

    8-24

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,19 +1,40 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
+#include "main.h"
 /**
-*main - Enter point
+* jack_bauer - es una funcion que imprimira cada minuto de las 24 horas.
 *
 *
 *
-*Retur: Always 0 (Success)
+*Retur: ... vacio.
 */
 
-int main(void)
+void jack_bauer(void)
 {
+int hrs;
+int min;
 
 
+for (hrs = 0; hrs <= 23; hrs++)
+{
+for (min = 0; min <= 59; min++)
+{
+int hrs_left = hrs / 10;
+int hrs_right = hrs % 10;
 
-return (0);
+int min_left = min / 10;
+int min_right = min % 10;
+
+
+_putchar(hrs_left + '0');
+_putchar(hrs_right + '0');
+
+_putchar(':');
+
+_putchar(min_left + '0');
+_putchar(min_right + '0');
+_putchar('\n');
+
+}
+
+}
+
 }

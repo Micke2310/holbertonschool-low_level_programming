c587568cd59380a835488978fedb1c9c9967808d
Micke2310 <maikyarachan@gmailcom>
2024-03-05 22:35:59 +0000

    9-times_table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,19 +1,49 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "main.h"
 
 /**
-*main - Enter point
+* times_table - imprimir la tabla del 9.
 *
 *
 *
-*Retur: Always 0 (Success)
+*Retur: ... vacio
 */
 
-int main(void)
+void times_table(void)
 {
+int left , up , result;
 
 
+for (left = 0; left <= 9 ; left++)
+{
+	for (up = 0; up <= 9; up++)
+	{
+	result = left * up;
+	if (left == 0)
+	{
+	_putchar('0');
+	} else if ( result < 10)
+	{
+	_putchar(',');
+	_putchar(' ');
+	_putchar(' ');
+	_putchar(result + '0');
+	}
+	else
+	{
+	_putchar(',');
+	_putchar(' ');
+	_putchar((result / 10) + '0');
+	_putchar((result % 10)+ '0');
+	
+	}
+	
+	
+	
+	
+	}
+_putchar('\n');
 
-return (0);
+}
+
+
 }

efba00d14ffcb6539b32c003125b2531d5cf5290
Micke2310 <maikyarachan@gmailcom>
2024-03-05 22:50:05 +0000

    9-times_table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,47 +1,39 @@
 #include "main.h"
 
 /**
-* times_table - imprimir la tabla del 9.
-*
-*
-*
-*Retur: ... vacio
-*/
+ * times_table - imprimir la tabla del 9.
+ *
+ * Return: vacío
+ */
 
 void times_table(void)
 {
-int left , up , result;
+    int left, up, result;
 
-
-for (left = 0; left <= 9 ; left++)
-{
-	for (up = 0; up <= 9; up++)
-	{
-	result = left * up;
-	if (left == 0)
-	{
-	_putchar('0');
-	} 
-	else if ( result < 10)
-	{
-	_putchar(',');
-	_putchar(' ');
-	_putchar(' ');
-	_putchar(result + '0');
-	}
-	else
-	{
-	_putchar(',');
-	_putchar(' ');
-	_putchar((result / 10) + '0');
-	_putchar((result % 10)+ '0');
-	
-	}
-	
-	}
-_putchar('\n');
-
-}
-
-
+    for (left = 0; left <= 9; left++)
+    {
+        for (up = 0; up <= 9; up++)
+        {
+            result = left * up;
+            if (left == 0)
+            {
+                _putchar('0');
+            }
+            else if (result < 10)
+            {
+                _putchar(',');
+                _putchar(' ');
+                _putchar(' ');
+                _putchar(result + '0');
+            }
+            else
+            {
+                _putchar(',');
+                _putchar(' ');
+                _putchar((result / 10) + '0');
+                _putchar((result % 10) + '0');
+            }
+        }
+        _putchar('\n');
+    }
 }

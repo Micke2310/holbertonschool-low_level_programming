4b8c4bfd70338769e21b1b81fc2d2d0dd58d0eda
Micke2310 <maikyarachan@gmail.com>
2024-03-08 05:09:17 +0000

    1-isdigit

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,30 @@
-#include "main.h"
+# include "main.h"
+
+/**
+ * _isdigit - funcion que verificara si el caracter es un digito (del 0 al 9).
+ * @c: es quien se chequea si es un digito
+ *
+ *Return: retorna 1 si es digito y 0 si no lo es.
+ */
 
 int _isdigit(int c)
 {
-for (c = 0; c >= 0 && c =< 9; c++);
+int d = 0;
+
+while (d >= 0 && d <= 9)
 {
-_putchar();
+if (c == d + '0')
+{
+_putchar('1');
+d++;
+return (1);
+}
+else
+{
+_putchar('0');
+d++;
 }
 
-
-
-
-
-
+}
+return (0);
 }

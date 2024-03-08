8e118595f4884fec59ac4de047935c17e1eb5aa3
Micke2310 <maikyarachan@gmail.com>
2024-03-08 05:56:46 +0000

    1-isdigit

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- *-isdigit - verifica si el valor es un digito del 0 al 9.
+ *_isdigit - esta funcion verifica si el valor es un digito del 0 al 9.
  *@c: es quien se compara
  *
- *Return: si es digito retorna 1 si no, 0.
- *
+ *Return: si es un digito retorna 1 si no lo es, retorna 0.
  */
 
 int _isdigit(int c)

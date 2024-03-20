ec4a015e619d77d93f379ac865c4b2b01ef3fb48
Micke2310 <maikyarachan@gmail.com>
2024-03-20 06:20:34 +0000

    practica_printf

--- practica_printf/practica_printf.c
+++ practica_printf/practica_printf.c
@@ -1,1 +1,15 @@
-codera luego
+ #include <stdio.h>
+
+int main( int argc, char* argv);
+{
+	char letra = 'a';
+	char *cadena = "%s";
+	int signedNum = -15;
+
+
+	printf("la variable vale %c.\n", letra);
+	
+	printf("la cadena vale %s.\n", cadena);
+	
+	printf("el numero vale %d.\n", letra);
+}

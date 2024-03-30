95e894d264322eaf4dc7d03d96d0604b0068de86
Micke2310 <maikyarachan@gmail.com>
2024-03-30 06:07:50 +0000

    C - Singly linked lists

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,1 +1,38 @@
-#include
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+
+/**
+ * print_list - prints all the elemnts of a list_t.
+ * @h: a pointer to the head of the list_t.
+ *
+ * return: the number of nodes in the list.
+ */
+
+
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;         /* size_t , tipo de dato sin signo, calcula tamaños de memoria*/
+			          /*,puede contener el numero maximo de unamatriz*/
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+		printf("[%d] (nil)\n", 0);
+		}
+		else
+		{
+		printf("[%d] %s\n", h->len, h->str);
+		}	
+		h = h->next;
+		coount++;
+	
+	}
+
+	return (count);
+}
+
+

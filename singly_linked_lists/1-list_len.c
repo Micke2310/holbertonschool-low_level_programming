0301d1162eed02c3f34761d0e808fd35330c64ca
Micke2310 <maikyarachan@gmail.com>
2024-03-30 07:39:24 +0000

    1-list_len.c

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -2,3 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
+
+/**
+ * list_len - Returns the number of elements in a linked list_t list.
+ * @h: A pointer to the head of the list_t list.
+ *
+ * Return: (count) The number of elements in the list.
+ */
+
+size_t list_len(const list_t *h)
+{
+    size_t count = 0; /*Inicializamos un contador para llevar el registro del número de nodo*/
+
+/*Bucle para recorrer la lista enlazada*/
+    while (h != NULL)
+    {
+        count++; /*Incrementamos el contador en cada iteració*/
+        h = h->next; /* Avanzamos al siguiente nodo*/
+    }
+
+    return count; /*Retornamos el número total de nodo*/
+}

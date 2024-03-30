2bda75793b7bc40cb58ffd8b77a0300cbdac69d1
Micke2310 <maikyarachan@gmail.com>
2024-03-30 23:46:41 +0000

    3-add_node_end.c

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -13,12 +13,12 @@
 
 size_t _strlen(const char *str)
 {
-        size_t len = 0;
+	size_t len = 0;
 
-        while (str[len] != '\0')
-        len++;
+	while (str[len] != '\0')
+	len++;
 
-        return (len);
+	return (len);
 }
 
 

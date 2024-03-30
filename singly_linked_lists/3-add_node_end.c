b93ec9ef376b62cae11ba1749595a32fb2bd48c6
Micke2310 <maikyarachan@gmail.com>
2024-03-30 23:30:49 +0000

    3-add_node_end.c

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -4,6 +4,33 @@
 #include "lists.h"
 
 
+/**
+ * _strlen - Returns the length of a string.
+ * @str: The string.
+ *
+ * Return: len
+ */
+
+size_t _strlen(const char *str)
+{
+        size_t len = 0;
+
+        while (str[len] != '\0')
+        len++;
+
+        return (len);
+}
+
+
+
+/**
+ * *add_node_end - Adds a new node at the end of a list_t.
+ * @head: pointer to a pointer to the head of the list_t.
+ * @str: string to add as the content of the new node.
+ *
+ * Return: If str is NULL, return NULL else,return the address the new element
+ */
+
 list_t *add_node_end(list_t **head, const char *str)
 {
 
@@ -23,7 +50,7 @@ list_t *add_node_end(list_t **head, const char *str)
 
 		if (new_n->str == NULL) /*cmprbams si duplicacion fue exitosa*/
 		{			/*si lo fue ,no entra, sino lo fue,*/
-		free(new_);		/*libera ell espacio resrvado cn FREE*/
+		free(new_n);		/*libera ell espacio resrvado cn FREE*/
 		return (NULL);		/*retorna NULL*/
 		}
 
@@ -43,10 +70,11 @@ list_t *add_node_end(list_t **head, const char *str)
 	last_n = *head;		/*si lista no es vacia, buscar ultimo nodo*/
 		while (last_n->next != NULL)
 		{
-		last_n = last_node->next;
+		last_n = last_n->next;
 		}
 
-	last_n->next = new_node;
+	last_n->next = new_n;	/*enlazar nuevo nodo al ultimo nodo*/
 
+return (new_n);
 
 }

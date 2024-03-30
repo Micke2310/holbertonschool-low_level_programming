6390107f4a65d276a57d4fb3f43c6bbfe03e1f6f
Micke2310 <maikyarachan@gmail.com>
2024-03-30 16:56:56 +0000

    2-add_node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -5,3 +5,20 @@
 
 
 list_t *add_node(list_t **head, const char *str);
+
+
+list_t *new_node;  /*declaramos un nuevo nodo*/
+
+new_node = malloc(sizeof(list_t)); /*asignamos memoria dinamica al nuevo nodo*/
+{
+	if (new_node == NULL ) /*verificamos si new_node fue creado*/
+	{
+	return (NULL);
+	}
+
+
+
+
+
+
+}

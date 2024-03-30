f3fb2f0f766352104effc55a0a2100e38d5c9ad6
Micke2310 <maikyarachan@gmail.com>
2024-03-30 20:28:34 +0000

    2-add_node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -4,6 +4,33 @@
 #include "lists.h"
 
 
+/**
+ * _strlen - Returns the length of a string.
+ * @str: The string.
+ *
+ * Return: len
+ */
+
+size_t _strlen(const char *str)
+{
+    size_t len = 0;
+
+    while (str[len] != '\0')
+        len++;
+
+    return len;
+}
+
+
+
+/**
+ * add_node - Adds a new node at the beginning of a list_t.
+ * @head: pointer to a pointer to the head of the list_t.
+ * @str: string to add as the content of the new node.
+ *
+ * Return: If str is NULL, return NULL else,return the address the new element
+ */
+
 list_t *add_node(list_t **head, const char *str)
 {
 
@@ -26,7 +53,8 @@ new_n->str = strdup(str);
 	return (NULL);
 	}
 
-new_n->len = (sizeof(str)); /*asignamos longitud de la cadena al nuevo nodo*/
+new_n->len = _strlen(str); /*asignamos longitud de la cadena al nuevo nodo*/
+/*funcion declarada en "list.h", "_strlen", ya que no puedo usar "strlen"*/
 
 new_n->next = *head; /*establecemos el siguiente nodo del nuevo nodo,*/
 			/*como el nodo actual de la cabeza de la lista*/

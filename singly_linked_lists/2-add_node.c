d0be84fcc698fe8d569fd9cea46695cba561acf0
Micke2310 <maikyarachan@gmail.com>
2024-03-30 18:07:05 +0000

    2-add_node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -16,9 +16,24 @@ new_node = malloc(sizeof(list_t)); /*asignamos memoria dinamica al nuevo nodo*/
 	return (NULL);
 	}
 
+new_node->str = strdup(str); 
+/*asignamos la cadena (str) a nuevo nodo y la duplicmos.*/
 
+	if (new_node->str == NULL) /*verificamos si se duplico str con exito*/
+	{
+	free(new_node); /*liberamos memoria dinamica asignada al nuevo nodo*/
+	return (NULL);
+	}
 
+new_node->leng = strlen(str); /*asignamos longitud de la cadena al nuevo nodo*/
 
+new_node->next = *head; /*establecemos el siguiente nodo del nuevo nodo,*/
+			/*como el nodo actual de la cabeza de la lista*/
+
+*head = new_node; /*actualizamos la cabeza de la lista para que apunte al*/
+			/*nuevo nodo */
+
+return (new_node); /*retornamos la direccion del nuevo nodo*/
 
 
 }

e57aea821cd593c493ae37778f58b26d2c5890c3
Micke2310 <maikyarachan@gmail.com>
2024-03-30 16:18:40 +0000

    2-add_node

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -4,19 +4,22 @@
 #include "lists.h"
 
 /**
- * list_len - Returns the number of elements in a linked list_t list.
- * @h: A pointer to the head of the list_t list.
+ * list_len - Returns the number of elements.
+ * @h: pointer to the head of the list_t.
  *
  * Return: (count) The number of elements in the list.
  */
 
 size_t list_len(const list_t *h)
 {
-size_t count = 0;/*Inicializamos un contador para llevar el registro del número de nodo*/
-		  
+size_t count = 0;
+/*Inicializamos un contador para llevar el registro del número d nodos*/
+
     while (h != NULL)
     {
-        count++; /*Incrementamos el contador en cada iteraci�*/
+        count++; /*Incrementamos el contador en cada iteracion*/
+	h = h->next;  /*avanzamos al ssiguiente nodo*/
     }
-    return (count); /*Retornamos el número total de nodo*/
+return (count); /*Retornamos el número tota*/
+
 }

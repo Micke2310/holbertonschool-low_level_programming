4a3d2b6b51b083f7fdbd2f6b1f3602dbff0588a1
Micke2310 <maikyarachan@gmail.com>
2024-03-30 20:50:20 +0000

    3-add_node_end.c

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,1 +1,19 @@
-#include
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+
+list_t *add_node_end(list_t ** head, const char *str)
+{
+
+list_t *new_n, *last_n;
+
+
+
+
+
+
+
+
+}

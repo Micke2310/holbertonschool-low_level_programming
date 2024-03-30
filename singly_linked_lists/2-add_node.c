1e52aaaeb372096e04664f296009017836ffe2e8
Micke2310 <maikyarachan@gmail.com>
2024-03-30 20:05:10 +0000

    2-add_node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -7,33 +7,34 @@
 list_t *add_node(list_t **head, const char *str)
 {
 
-list_t *new_node;  /*declaramos un nuevo nodo*/
+list_t *new_n;  /*declaramos un nuevo nodo*/
 
-new_node = malloc(sizeof(list_t)); /*asignamos memoria dinamica al nuevo nodo*/
+new_n = malloc(sizeof(list_t)); /*asignamos memoria dinamica al nuevo nodo*/
 
-	if (new_node == NULL ) /*verificamos si new_node fue creado*/
+	if (new_n == NULL ) /*verificamos si new_n fue creado*/
 	{
 	return (NULL);
 	}
 
-new_node->str = strdup(str); 
+new_n->str = strdup(str); 
 /*asignamos la cadena (str) a nuevo nodo y la duplicmos.*/
 
-	if (new_node->str == NULL) /*verificamos si se duplico str con exito*/
+	if (new_n->str == NULL) /*verificamos si se duplico str con exito*/
 	{
-	free(new_node); /*liberamos memoria dinamica asignada al nuevo nodo*/
+	free(new_n); /*liberamos memoria dinamica asignada al nuevo nodo*/
+	/*si no utilizaremos mas, esa cadena, de lo contrario, no liberar*/
 	return (NULL);
 	}
 
-new_node->len = strlen(str); /*asignamos longitud de la cadena al nuevo nodo*/
+new_n->len = (sizeof(str)); /*asignamos longitud de la cadena al nuevo nodo*/
 
-new_node->next = *head; /*establecemos el siguiente nodo del nuevo nodo,*/
+new_n->next = *head; /*establecemos el siguiente nodo del nuevo nodo,*/
 			/*como el nodo actual de la cabeza de la lista*/
 
-*head = new_node; /*actualizamos la cabeza de la lista para que apunte al*/
+*head = new_n; /*actualizamos la cabeza de la lista para que apunte al*/
 			/*nuevo nodo */
 
-return (new_node); /*retornamos la direccion del nuevo nodo*/
+return (new_n); /*retornamos la direccion del nuevo nodo*/
 
 
 }

1016ea8ba8c3baf4574b5f8e576494d32b2928a8
Micke2310 <maikyarachan@gmail.com>
2024-03-31 01:16:34 +0000

    4-free_list.c

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -8,5 +8,6 @@ void print_before_main(void) __attribute__((constructor));
  */
 void print_before_main(void)
 {
-printf("You're beat! and yet,you must allow,\nI bore my house uponmy back!\n");
+printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
 }
